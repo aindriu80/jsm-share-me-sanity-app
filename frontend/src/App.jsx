5d5c5a7a5a335afcdcfe26590f519380ac69209c
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-07-05 11:42:46 +0100

    Simple Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
 import logo from './logo.svg'
 import './App.css'
+import Login from './components/Login'
+import Home from './container/Home'
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">
-        React and Tailwind with Vitejs!
-      </h1>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
